1c5ddf7c5429bde03c7c467cfd9557d8b7cc0f45
HP <saiswaroopasaranath1211@gmail.com>
2023-03-23 12:27:50 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace UserDefineException
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //int age = 17;
+            int age = Convert.ToInt32(Console.ReadLine());  
+            try
+            {
+                CheckAge(age);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            /*
+            catch(MyException e)
+            {
+                Console.WriteLine(e.Message);
+            }*/
+        }
+        public static void CheckAge(int age)
+        {
+            if (age < 18) {
+                throw new MyException("Not Eligible");
+            //Console.WriteLine("Not Eligible")
+            //
+            }
+        }
+        public class MyException : Exception
+        {
+            public MyException(string myMessage):base(myMessage) { 
+            }
+        }
+    }
+}
